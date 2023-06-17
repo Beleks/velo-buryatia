7b16fbcf311c45822b932f4ed217276b62a46532
Aleksey Beletsky <lexa.beletsky.b@gmail.com>
2023-06-17 17:11:39 +0700

    Add icons, delay column, some categories

--- src/pages/Season.vue
+++ src/pages/Season.vue
@@ -89,89 +89,141 @@ function selectTypeBike(type) {
 function selectGroup(group) {
   selectedGroup.value = group.value;
 }
+
+function culcDelay(sourceTime) {
+  if (!sourceTime) {
+    return;
+  }
+
+  let time = {};
+  let bestTime = participants.value[0].time;
+  let delay = {
+    hour: 0,
+    minute: 0,
+    sec: 0,
+  };
+
+  Object.assign(time, sourceTime);
+
+  delay.sec = () => {
+    if (time.sec < bestTime.sec) {
+      time.minute -= 1;
+      time.sec += 60;
+    }
+    return time.sec - bestTime.sec < 10
+      ? `0${time.sec - bestTime.sec}`
+      : time.sec - bestTime.sec;
+  };
+  delay.minute = () => {
+    if (time.minute < bestTime.minute) {
+      time.hour -= 1;
+      time.minute += 60;
+    }
+    return time.minute - bestTime.minute < 10
+      ? `0${time.minute - bestTime.minute}`
+      : time.minute - bestTime.minute;
+  };
+  delay.hour = time.hour - bestTime.hour;
+
+  return `+ ${delay.hour}:${delay.minute()}:${delay.sec()}`;
+}
+
+function formatTime(time) {
+  // hour:minute:sec 00:00:00
+  return `${time.hour}:${time.minute < 10 ? `0${time.minute}` : time.minute}:${
+    time.sec < 10 ? `0${time.sec}` : time.sec
+  }`;
+}
 </script>
 
 <template>
   <div class="px-24 max-w-7xl m-auto">
-    <div class="flex justify-between items-center mb-6 mx-6">
-      <router-link :to="{ name: 'Results' }"> Назад </router-link>
-      <span class="text-2xl font-extrabold">
-        Байкальский веломарафон {{ selectedSeason }}
-      </span>
-      <div class="opacity-0 cursor-default" :to="{ name: 'Results' }">
-        Назад
+    <template v-if="seasonMarathon">
+      <div class="flex justify-between items-center mb-6 mx-6">
+        <router-link :to="{ name: 'Results' }"> Назад </router-link>
+        <span class="text-2xl font-extrabold">
+          Байкальский веломарафон {{ selectedSeason }}
+        </span>
+        <div class="opacity-0 cursor-default">Назад</div>
       </div>
-    </div>
-    <div class="px-4 mb-5 flex justify-between">
-      <div class="flex items-center">
+      <div class="px-4 mb-5 flex justify-between">
         <div class="flex items-center">
-          <div class="opacity-60 mr-3">Дистанция:</div>
-          <InputSelect :options="distances" @input="selectDistance" />
+          <div class="flex items-center">
+            <div class="opacity-60 mr-3">Дистанция:</div>
+            <InputSelect :options="distances" @input="selectDistance" />
+          </div>
+          <div class="flex items-center">
+            <div class="opacity-60 mr-3">Тип велосипеда:</div>
+            <InputSelect :options="typesBike" @input="selectTypeBike" />
+          </div>
+          <div class="flex items-center">
+            <div class="opacity-60 mr-3">Группа:</div>
+            <InputSelect :options="groups" @input="selectGroup" :width="160" />
+          </div>
         </div>
-        <div class="flex items-center">
-          <div class="opacity-60 mr-3">Тип велосипеда:</div>
-          <InputSelect :options="typesBike" @input="selectTypeBike" />
-        </div>
-        <div class="flex items-center">
-          <div class="opacity-60 mr-3">Группа:</div>
-          <InputSelect :options="groups" @input="selectGroup" :width="160" />
-        </div>
-      </div>
-      <div>
-        <div
-          class="border my-border-color rounded px-2 py-1 bg-input-color cursor-pointer hover:border-lime-400 transition ease-out"
-        >
-          <!-- Глаз svg -->
-          <span class="opacity-40">Общий зачёт</span>
-        </div>
-      </div>
-    </div>
-    <div class="border my-border-color rounded">
-      <div
-        class="bg-table-color justify-between px-4 py-2 flex font-normal items-center opacity-80"
-      >
-        <div class="flex items-center">
-          <div class="w-7 h-7 mr-4 flex justify-center items-center">#</div>
-          <div class="w-7 text-center mr-2">№</div>
-          <div class="w-60 mr-4">Имя Фамилия</div>
-          <div class="w-40 mr-4">Город</div>
-          <div class="w-40 mr-4">Команда</div>
-        </div>
-        <div class="flex items-center">
-          <div class="w-20 mr-2 text-end"></div>
-          <div class="w-20 text-end">Время</div>
-        </div>
-      </div>
-      <div
-        class="flex items-center justify-between px-4 border-b last:border-none my-border-color py-2"
-        v-for="(participant, index) in participants"
-        :key="participant.place"
-      >
-        <div class="flex items-center">
+        <div>
           <div
-            :class="[
-              { first: index == 0, second: index == 1, third: index == 2 },
-              'text-lg font-bold w-7 h-7 flex justify-center items-center rounded-full mr-4',
-            ]"
+            class="border my-border-color rounded px-2 py-1 bg-input-color cursor-pointer hover:border-lime-400 transition ease-out"
           >
-            {{ index + 1 }}
-          </div>
-          <div class="w-7 text-xs bg-my-color rounded text-center mr-2">
-            {{ participant.number }}
-          </div>
-          <div class="w-60 mr-4">{{ participant.name }}</div>
-          <div class="w-40 mr-4">{{ participant.city }}</div>
-          <div class="w-40 mr-4">-</div>
-        </div>
-        <div class="flex items-center">
-          <div class="w-20 mr-2 opacity-70 text-end">
-            {{ participant.delay }}
-          </div>
-          <div class="w-20 text-end">
-            {{ participant.time ? participant.time : "Сошёл" }}
+            <!-- Глаз svg -->
+            <span class="opacity-40">Общий зачёт</span>
           </div>
         </div>
       </div>
+      <div class="border my-border-color rounded">
+        <div
+          class="bg-table-color justify-between px-4 py-2 flex font-normal items-center opacity-80"
+        >
+          <div class="flex items-center">
+            <div class="w-7 h-7 mr-4 flex justify-center items-center">#</div>
+            <div class="w-7 text-center mr-2">№</div>
+            <div class="w-60 mr-4">Имя Фамилия</div>
+            <div class="w-40 mr-4">Город</div>
+            <div class="w-40 mr-4">Команда</div>
+          </div>
+          <div class="flex items-center">
+            <div class="w-20 mr-2 text-end"></div>
+            <div class="w-20 text-end">Время</div>
+          </div>
+        </div>
+        <div
+          class="flex items-center justify-between px-4 border-b last:border-none my-border-color py-2"
+          v-for="(participant, index) in participants"
+          :key="participant.place"
+        >
+          <div class="flex items-center">
+            <div
+              :class="[
+                {
+                  first: participant.place == 1,
+                  second: participant.place == 2,
+                  third: participant.place == 3,
+                },
+                'text-lg font-bold w-7 h-7 flex justify-center items-center rounded-full mr-4',
+              ]"
+            >
+              {{ participant.place ? participant.place : "-" }}
+            </div>
+            <div class="w-7 text-xs bg-my-color rounded text-center mr-2">
+              {{ participant.number }}
+            </div>
+            <div class="w-60 mr-4">{{ participant.name }}</div>
+            <div class="w-40 mr-4">{{ participant.city }}</div>
+            <div class="w-40 mr-4">-</div>
+          </div>
+          <div class="flex items-center">
+            <div class="w-20 mr-2 opacity-70 text-end">
+              {{ participant.place !== 1 ? culcDelay(participant.time) : "" }}
+            </div>
+            <div class="w-20 text-end">
+              {{ participant.time ? formatTime(participant.time) : "Сошёл" }}
+            </div>
+          </div>
+        </div>
+      </div></template
+    >
+    <div v-else class="text-2xl font-extrabold text-center">
+      Нет результатов
     </div>
   </div>
 </template>
