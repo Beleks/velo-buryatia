a4c845500e2ce2831aa1640620e6a352fd515d1b
Aleksey Beletsky <aleksey.beletsky.av@gmail.com>
2023-09-20 21:42:01 +0800

    Update InputSelect.vue

--- src/components/InputSelect.vue
+++ src/components/InputSelect.vue
@@ -43,22 +43,16 @@ watch(
 );
 
 onClickOutside(target, (event) => {
-
   // Не работает из-за "hover:"
   // console.log(
   //   event.target.closest(`.${[...ignorEl.value.classList].join(".")}`),
   //   "closest"
   // );
   //
-  if (
-    ignorEl.value ==
-    event.target.closest(
-      `.border.my-border-color.rounded.px-2.py-1.bg-input-color`
-    )
-  ) {
+  if (ignorEl.value == event.target.closest(`.border.my-border-color.rounded.px-2.py-1.bg-input-color`)) {
     return;
   }
-  closeDropdownMenu(event);
+  closeDropdownMenu();
 });
 
 function switchDropdownMenu() {
@@ -68,7 +62,7 @@ function switchDropdownMenu() {
   dropdownMenuIsOpen.value = !dropdownMenuIsOpen.value;
 }
 
-function closeDropdownMenu(event) {
+function closeDropdownMenu() {
   dropdownMenuIsOpen.value = false;
 }
 
@@ -80,41 +74,23 @@ function selectOption(option) {
 
 <template>
   <div class="">
-    <div
-      :style="{ width: `${width}px` }"
+    <div :style="{ width: `${width}px` }"
       class="border my-border-color rounded px-2 py-1 bg-input-color transition ease-out flex justify-between items-center stroke-white"
-      :class="[disable ? 'opacity-50' : 'hover:border-lime-400 cursor-pointer']"
-      @click="switchDropdownMenu"
-      ref="ignorEl"
-    >
+      :class="[disable ? 'opacity-50' : 'hover:border-lime-400 cursor-pointer']" @click="switchDropdownMenu"
+      ref="ignorEl">
       <div>{{ selectedOption }}</div>
-      <SimpleArrowSvg
-        :class="[dropdownMenuIsOpen ? 'rotate-180' : '', 'transition ease-out']"
-        :size="20"
-      />
+      <SimpleArrowSvg :class="[dropdownMenuIsOpen ? 'rotate-180' : '', 'transition ease-out']" :size="20" />
     </div>
-    <div
-      v-if="dropdownMenuIsOpen"
-      :style="{ width: `${width}px` }"
-      ref="target"
-      class="absolute mt-2 border my-border-color rounded z-10 shadow-md flex flex-col overflow-hidden"
-    >
-      <span
-        :class="[
-          'px-2 py-1 cursor-pointer hover:text-lime-400',
-          option == selectedOption
-            ? 'bg-input-color-selected'
-            : 'bg-input-color',
-        ]"
-        v-for="option in options"
-        :key="option"
-        @click="selectOption(option)"
-      >
+    <div v-if="dropdownMenuIsOpen" :style="{ width: `${width}px` }" ref="target"
+      class="absolute mt-2 border my-border-color rounded z-10 shadow-md flex flex-col overflow-hidden">
+      <span :class="[
+        'px-2 py-1 cursor-pointer hover:text-lime-400',
+        option == selectedOption ? 'bg-input-color-selected' : 'bg-input-color',
+      ]" v-for="option in options" :key="option" @click="selectOption(option)">
         {{ option }}
       </span>
     </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
