fe6b21dd2e681886b154eec4624f3dc5144d38f3
Aleksey Beletsky <aleksey.beletsky.av@gmail.com>
2025-01-11 01:57:11 +0800

    Remove empty settings object

--- src/stores/MainStore.js
+++ src/stores/MainStore.js
@@ -3,9 +3,8 @@ import { ref } from "vue";
 import axios from "axios";
 
 export const useMainStore = defineStore("main", () => {
-  // TODO: ?
-  let apiOptions;
-  // ?
+  // TODO: Вынести базовые настройки?
+  // let apiOptions;
 
   let protocols = ref([
     // ID соревнования смотреть в БД, таблица events
@@ -44,31 +43,20 @@ export const useMainStore = defineStore("main", () => {
   ]);
 
   async function getEvents() {
-    return await axios.get("st/counts/ev_cyclists?tag_id=1", apiOptions);
+    return await axios.get("st/counts/ev_cyclists?tag_id=1");
   }
 
   async function getEventResults(eventId) {
-    apiOptions = {
-      // headers: { Authorization: `Bearer ${mainStore.accessToken}` },
-    };
-    return await axios.get(`results/${eventId}`, apiOptions);
+    return await axios.get(`results/${eventId}`);
   }
 
-  // TODO: Поменять на participant
+  // TODO: Поменять название на participant?
   async function getCyclists() {
-    apiOptions = {
-      // headers: { Authorization: `Bearer ${mainStore.accessToken}` },
-    };
-    //apivelo.bielecki.ru/api/st/list/cyclist_events?tag_id=1&cyclist_id=121
-    return await axios.get(`st/list/ev_cyclists?tag_id=1`, apiOptions);
+    return await axios.get(`st/list/ev_cyclists?tag_id=1`);
   }
 
   async function getCyclistsResults(cyclistId) {
-    apiOptions = {
-      // headers: { Authorization: `Bearer ${mainStore.accessToken}` },
-    };
-    //apivelo.bielecki.ru/api/st/list/cyclist_events?tag_id=1&cyclist_id=121
-    return await axios.get(`st/list/cyclist_events?tag_id=1&cyclist_id=${cyclistId}`, apiOptions);
+    return await axios.get(`st/list/cyclist_events?tag_id=1&cyclist_id=${cyclistId}`);
   }
 
   return {
